996a1b6c62459ee4b4cae00e207244fc5bff1fda
sergii13 <sergii.khilchuk13@gmail.com>
2024-05-23 00:44:25 +0300

    fix bug

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,1 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {});
+import { Application } from "@splinetool/runtime";
+
+const canvas = document.getElementById("canvas");
+const app = new Application(canvas);
+app.load("https://prod.spline.design/20dGyTTufZ-vZ9Ja/scene.splinecode");
