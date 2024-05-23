832d082523828ac3bfa3818113e1a12e6e77d9d3
sergii13 <sergii.khilchuk13@gmail.com>
2024-05-23 10:30:25 +0300

    fix

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,6 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {});
 import { Application } from "@splinetool/runtime";
 
 const canvas = document.getElementById("canvas");
 const app = new Application(canvas);
 app.load("https://prod.spline.design/20dGyTTufZ-vZ9Ja/scene.splinecode");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const list = document.querySelector(".header__list");
+//   const allElement = list.querySelectorAll("a svg, a span");
+//   list.addEventListener("mouseover", (e) => {
+//     allElement.forEach((el) => {
+//       console.log(el);
+//       el.style.animation = "none";
+//       el.offsetHeight;
+//       el.style.animation = null;
+//       el.style.transition = "0.5s easy";
+//     });
+//   });
+// });
