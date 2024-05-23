4ac40c7bf8caa60006d4fda17f0454e8765f8667
sergii13 <sergii.khilchuk13@gmail.com>
2024-05-23 15:57:56 +0300

    fix

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,8 +1,8 @@
-import { Application } from "@splinetool/runtime";
-
-const canvas = document.getElementById("canvas");
-const app = new Application(canvas);
-app.load("https://prod.spline.design/20dGyTTufZ-vZ9Ja/scene.splinecode");
+// import { Application } from "@splinetool/runtime";
+//
+// const canvas = document.getElementById("canvas");
+// const app = new Application(canvas);
+// app.load("https://prod.spline.design/20dGyTTufZ-vZ9Ja/scene.splinecode");
 // document.addEventListener("DOMContentLoaded", () => {
 //   const list = document.querySelector(".header__list");
 //   const allElement = list.querySelectorAll("a svg, a span");
