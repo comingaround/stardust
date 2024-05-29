b1174d51c768f67cdcee04edf67e3dfb980722c0
comingaround <131857839+comingaround@users.noreply.github.com>
2024-05-29 15:58:28 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,38 +2,36 @@ import "../scss/style.scss";
 import "virtual:svg-icons-register";
 import "https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.4/lottie.min.js";
 
-// Load the Lottie animation
-const animationContainer = document.getElementById('lottie');
-const animationDataUrl = '/src/lotties/idle-star.json'; // Updated local file path
+// Function to load Lottie animations
+function loadLottieAnimation(containerId, animationPath, loop = true, autoplay = true, speed = 1) {
+    const container = document.getElementById(containerId);
+    if (container) {
+        const animation = lottie.loadAnimation({
+            container: container,
+            renderer: 'svg',
+            loop: loop,
+            autoplay: autoplay,
+            path: animationPath
+        });
+        animation.setSpeed(speed);
+        return animation;
+    }
+    console.error(`Container with id ${containerId} not found.`);
+    return null;
+}
 
-// Load main animation
-const mainAnimation = lottie.loadAnimation({
-    container: animationContainer,
-    renderer: 'svg',
-    loop: 2,
-    autoplay: true,
-    path: animationDataUrl
-});
+// Load the main animation
+loadLottieAnimation('lottie', '/src/lotties/idle-star.json', 2, true, 1.75);
 
-// Set initial playback speed
-mainAnimation.setSpeed(1.75);
-
-// Load star menu animation
-const starMenuAnimationContainer = document.getElementById('idle-star--menu');
-const animationMenu = lottie.loadAnimation({
-    container: starMenuAnimationContainer,
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: animationDataUrl
-});
-
-animationMenu.setSpeed(1.5);
+// Load the star menu animation
+const animationMenu = loadLottieAnimation('idle-star--menu', '/src/lotties/idle-star.json', true, true, 1.5);
 
 // Pause the animation menu for 4 seconds after each loop
-animationMenu.addEventListener('loopComplete', () => {
-    animationMenu.pause();
-    setTimeout(() => {
-        animationMenu.play();
-    }, 4000); // 4000 milliseconds = 4 seconds
-});
+if (animationMenu) {
+    animationMenu.addEventListener('loopComplete', () => {
+        animationMenu.pause();
+        setTimeout(() => {
+            animationMenu.play();
+        }, 4000); // 4000 milliseconds = 4 seconds
+    });
+}
